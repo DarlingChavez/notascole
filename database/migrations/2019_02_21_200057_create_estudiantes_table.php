7630e812d041049296801c2cacbe2bc5f29ff1c2
Darling Chavez <darling.chavez@hotmail.com>
2019-02-21 16:20:02 -0500

    create table estudiantes

---
+++ database/migrations/2019_02_21_200057_create_estudiantes_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEstudiantesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('estudiantes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('cedula')->unique();
+            $table->string('nombres');
+            $table->string('apellidopaterno');
+            $table->string('apellidomaterno');
+            $table->integer('curso_id')->unsigned();
+            $table->foreign('curso_id')->references('id')->on('cursos');
+            $table->string('email')->unique();
+            $table->date('nacimiento')->nullable();
+            $table->string('telefono')->nullable();
+            $table->timestamps();
+            $table->string('updatedbyuser')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('estudiantes');
+    }
+}
