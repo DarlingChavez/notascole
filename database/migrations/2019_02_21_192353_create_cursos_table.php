5ee84b3b72e29c96be9949df5bf40d3b53b21c43
Darling Chavez <darling.chavez@hotmail.com>
2019-02-21 14:56:44 -0500

    create cursos table

---
+++ database/migrations/2019_02_21_192353_create_cursos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCursosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cursos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->tinyInteger('grado');
+            $table->string('paralelo');
+            $table->string('nivel');
+            $table->string('descripcion');
+            $table->char('estado',1);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cursos');
+    }
+}
